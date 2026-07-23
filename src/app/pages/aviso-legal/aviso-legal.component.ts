1ca052ed2b4a91e7314c1b728d7636ee7508c917
raul.perea <raul.perea@abbvie.com>
2026-07-23 09:47:17 +0200

    avances v1

--- src/app/pages/aviso-legal/aviso-legal.component.ts
+++ src/app/pages/aviso-legal/aviso-legal.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-aviso-legal',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, MatIcon, MatButtonModule],
   templateUrl: './aviso-legal.component.html',
   styleUrl: './aviso-legal.component.css',
 })
 export class AvisoLegalComponent {
-  fechaActualizacion = '18 de julio de 2026';
+  fechaActualizacion = '23 de julio de 2026';
 
   constructor(private title: Title) {
     this.title.setTitle('Aviso Legal | Paraíso Para Saborear');
